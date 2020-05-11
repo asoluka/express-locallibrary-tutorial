32032a80ee70bfb5b870c833f997f01e7c55f53d
TC Asoluka <stracool9@gmail.com>
2020-05-11 15:40:52 +0100

    First version of application moved into github

---
+++ models/author.js
@@ -0,0 +1,53 @@
+var mongoose = require('mongoose');
+var moment = require('moment');
+
+var Schema = mongoose.Schema;
+var AuthorSchema = new Schema(
+    {
+        first_name: {type: String, required: true, max: 100},
+        family_name: {type: String, required: true, max: 100},
+        date_of_birth: {type: Date},
+        date_of_death: {type: Date},
+    }
+);
+
+// Virtual for author's full name
+AuthorSchema.virtual('name')
+.get(function () {
+    // To avoid errors in cases where an author does not have either a family name or a firstname.
+    // We want to make sure we handle the exception by returning an empty string for that case
+
+    var fullname = '';
+    if (this.first_name && this.family_name) {
+        fullname = this.family_name + ', ' + this.first_name
+    }
+    if (!this.first_name || !this.family_name) {
+        fullname = '';
+    }
+
+    return fullname;
+});
+
+// virtual for author's lifespan
+AuthorSchema
+.virtual('lifespan')
+.get(function () {
+    return this.date_of_birth ? moment(this.date_of_birth).format('YYYY-MM-DD') : '';
+});
+
+//Virtual for author's date of death
+AuthorSchema
+.virtual('dateofdeath')
+.get(function() {
+    return this.date_of_death ? moment(this.date_of_death).format('YYYY-MM-DD') : '';
+});
+
+// Virtual for author's URL
+AuthorSchema
+.virtual('url')
+.get(function () {
+    return '/catalog/author/' + this.id;
+});
+
+// export model
+module.exports = mongoose.model('Author', AuthorSchema);
